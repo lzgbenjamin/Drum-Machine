ca6fcac8e4ce53e00e785aaaef12af650ea43ffe
yungbentai <74857832+yungbentai@users.noreply.github.com>
2021-08-12 19:29:25 +0800

    FCC Drum Machine

---
+++ index.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="text-center">Hello</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
